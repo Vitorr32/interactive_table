965c61c1b1f1d53b4420f666aa9f03879d7a1f66
Vitor Rodrigues Costa <vitor_costa@virgo.atlantico.net.br>
2019-06-19 18:20:51 -0300

    Updates

---
+++ precache-manifest.18af45d1f6a58c65cc44d7010a8ff12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62381eeaea5ccc0d90ace215043f0c20",
+    "url": "/interactive_table/index.html"
+  },
+  {
+    "revision": "77538372473d083e1d17",
+    "url": "/interactive_table/static/css/main.e3aab934.chunk.css"
+  },
+  {
+    "revision": "53ac77ed38ec6c989a94",
+    "url": "/interactive_table/static/js/2.1d99af90.chunk.js"
+  },
+  {
+    "revision": "77538372473d083e1d17",
+    "url": "/interactive_table/static/js/main.bc8d336e.chunk.js"
+  },
+  {
+    "revision": "b2716960dff8050033f6",
+    "url": "/interactive_table/static/js/runtime~main.76204184.js"
+  }
+]);
